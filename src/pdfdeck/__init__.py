e1d8d3f4470378086e5eb9606ff96e50c6be7b49
User <user@example.com>
2026-01-22 07:42:57 +0100

    release: v2.0.2

--- src/pdfdeck/__init__.py
+++ src/pdfdeck/__init__.py
@@ -4,6 +4,6 @@ PDFDeck - Zaawansowana aplikacja desktopowa do manipulacji PDF.
 Profesjonalne narzędzie dla prawników, HR, urzędów i firm.
 """
 
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 __author__ = "PDFDeck Team"
 __app_name__ = "PDFDeck"
